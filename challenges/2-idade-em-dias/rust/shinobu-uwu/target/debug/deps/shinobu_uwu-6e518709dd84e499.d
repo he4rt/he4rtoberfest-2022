ab4b65e353ff9ee3868222f09d365b372a1206d4
Matheus Filipe dos Santos Reinert <matheus.reinert@protonmail.com>
2022-10-03 20:23:58 -0300

    Implementação exercício 2

---
+++ challenges/2-idade-em-dias/rust/shinobu-uwu/target/debug/deps/shinobu_uwu-6e518709dd84e499.d
@@ -0,0 +1,7 @@
+/home/shinobu/Projects/Rust/he4rtoberfest-2022/challenges/2-idade-em-dias/rust/shinobu-uwu/target/debug/deps/shinobu_uwu-6e518709dd84e499.rmeta: src/main.rs
+
+/home/shinobu/Projects/Rust/he4rtoberfest-2022/challenges/2-idade-em-dias/rust/shinobu-uwu/target/debug/deps/shinobu_uwu-6e518709dd84e499.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=
