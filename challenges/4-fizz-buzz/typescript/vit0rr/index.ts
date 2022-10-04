ebb3ec5f3936c7cafecce6609117c42372f2fbe7
Vitor <vi.souza.almeida@protonmail.com>
2022-10-03 22:41:36 -0300

    Finalizando o desafio

---
+++ challenges/4-fizz-buzz/typescript/vit0rr/index.ts
@@ -0,0 +1,15 @@
+function fizzBuzz(x: number) {
+  for (let i = 1; i <= x; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz(16);
