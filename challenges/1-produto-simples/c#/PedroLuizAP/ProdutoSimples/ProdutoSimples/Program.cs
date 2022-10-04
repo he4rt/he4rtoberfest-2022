d02aa31110af05bed0e912546b9d87e3bc4c3a29
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-10-04 10:40:31 -0300

    Finalizando o desafio

---
+++ challenges/1-produto-simples/c#/PedroLuizAP/ProdutoSimples/ProdutoSimples/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int firstValue = 0;
+        int secondValue = 0;
+
+        if (int.TryParse(args[0], out firstValue) && int.TryParse(args[1], out secondValue))
+            Console.WriteLine($"Produto: {firstValue * secondValue}");
+    }
+}
