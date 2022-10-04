582331262bef15803917a1154378b63fa8a5ef9e
rinyaresu <kaiquelinhares17@outlook.com>
2022-10-03 23:33:29 -0300

    Finalizando o desafio 1

---
+++ challenges/1-produto-simples/ruby/rinyaresu/desafio-1.rb
@@ -0,0 +1,17 @@
+class Soma
+  def initialize
+    @PROD = var * var2    puts "PROD = #{@PROD}"
+  end
+
+  def var
+    puts "Escreva um numero"
+    var1 = gets.to_i
+  end
+
+  def var2
+    puts "Escreva outro numero"
+    var2 = gets.to_i
+  end
+end
+
+soma = Soma.new
