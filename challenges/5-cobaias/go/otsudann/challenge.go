f2237bcb696acc4ce69609e009aac7b323aca526
Daniel Augusto <daniel.aug.souza@gmail.com>
2022-10-04 17:03:07 -0300

    Solution challenge 5

---
+++ challenges/5-cobaias/go/otsudann/challenge.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+    "fmt"
+    s "strings"
+)
+
+func main(){
+
+  //testeManual()
+  testeAutomatico()
+
+}
+
+func testeManual(){
+  var qtdTestes int
+  var cobaia string
+  var qtdCobaia float64
+  var totalSapos float64
+  var totalCoelhos float64
+  var totalRatos float64
+
+  fmt.Println("Quantos testes foram realizados?")
+  fmt.Scan(&qtdTestes)
+
+  for i := 0; i < qtdTestes; i++ {
+    fmt.Printf("Teste: %v\n", i)
+    fmt.Println("Insira 'quantidade cobaia' sendo cobaias R/S/C")
+    fmt.Scan(&qtdCobaia, &cobaia)
+
+    if s.ToUpper(cobaia) == "R"{
+      totalRatos += qtdCobaia
+    }else if s.ToUpper(cobaia) == "S"{
+      totalSapos += qtdCobaia
+    }else if s.ToUpper(cobaia) == "C"{
+      totalCoelhos += qtdCobaia
+    }
+  }
+
+  exibirTotal(totalRatos, totalSapos, totalCoelhos)
+}
+
+func testeAutomatico(){
+  var qtdCobaiasArray = [10]float64{10, 6, 15, 5, 14, 9, 6, 8, 5, 14}
+  var CobaiasArray = [10]string{"C", "R", "S", "C", "R", "C", "R", "S", "C", "R"}
+  var totalSapos float64
+  var totalCoelhos float64
+  var totalRatos float64
+
+  for i := 0; i < len(CobaiasArray); i++ {
+    if s.ToUpper(CobaiasArray[i]) == "R"{
+      totalRatos += qtdCobaiasArray[i]
+    }else if s.ToUpper(CobaiasArray[i]) == "S"{
+      totalSapos += qtdCobaiasArray[i]
+    }else if s.ToUpper(CobaiasArray[i]) == "C"{
+      totalCoelhos += qtdCobaiasArray[i]
+    }
+  }
+  exibirTotal(totalRatos, totalSapos, totalCoelhos)
+}
+
+func exibirTotal(totalRatos float64, totalSapos float64, totalCoelhos float64){
+  var totalCobaias float64
+  totalCobaias = totalRatos + totalSapos + totalCoelhos
+
+  fmt.Println("Total: \t\t\t", totalCobaias)
+  fmt.Println("Total Ratos: \t\t", totalRatos)
+  fmt.Println("Total Sapos: \t\t", totalSapos)
+  fmt.Println("Total Coelhos: \t\t", totalCoelhos)
+  fmt.Printf("Percentual Ratos: \t%.2f\n", (totalRatos * 100) / totalCobaias)
+  fmt.Printf("Percentual Sapos: \t%.2f\n", (totalSapos * 100) / totalCobaias)
+  fmt.Printf("Percentual Coelhos: \t%.2f\n", (totalCoelhos * 100) / totalCobaias)
+}
