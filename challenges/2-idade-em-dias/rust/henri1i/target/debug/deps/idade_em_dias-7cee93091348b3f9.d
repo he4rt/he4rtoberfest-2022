33e30c06466114b582234fe235498f959240f216
henri <henri1i.dev@gmail.com>
2022-10-04 20:16:15 -0300

    feat: finish challange 2

---
+++ challenges/2-idade-em-dias/rust/henri1i/target/debug/deps/idade_em_dias-7cee93091348b3f9.d
@@ -0,0 +1,5 @@
+/home/henri/he4rtoberfest-2022/challenges/2-idade-em-dias/rust/henri1i/target/debug/deps/idade_em_dias-7cee93091348b3f9: src/main.rs
+
+/home/henri/he4rtoberfest-2022/challenges/2-idade-em-dias/rust/henri1i/target/debug/deps/idade_em_dias-7cee93091348b3f9.d: src/main.rs
+
+src/main.rs:
