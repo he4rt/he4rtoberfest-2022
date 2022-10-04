dda591ab4483d31eca78ee4b01510a598abaf8fd
Nayla Gomes <nayla.cgs@gmail.com>
2022-10-04 12:26:55 -0300

    Tirando arquivos desafio 1

--- challenges/1-produto-simples/python/naygo/produto-simples.py
+++
@@ -1,5 +0,0 @@
-print('Digite dois valores inteiros para ver o produto entre eles.')
-primeiro = int(input('Digite o primeiro valor: '))
-segundo = int(input('Digite o segundo valor: '))
-PROD = primeiro * segundo
-print('produto: {}'.format(PROD))
