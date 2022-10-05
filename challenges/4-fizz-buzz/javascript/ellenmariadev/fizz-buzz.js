31fd10d19b6ea91da022bcf497a56d71e0a491ff
ellenmariadev <ellenmariadev@gmail.com>
2022-10-05 02:24:38 -0300

    Finalizando o desafio

---
+++ challenges/4-fizz-buzz/javascript/ellenmariadev/fizz-buzz.js
@@ -0,0 +1,21 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question(`Digite um número: `, (valor) => {
+  for (i = 1; i <= valor; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+  readline.close()
+});
+
+
