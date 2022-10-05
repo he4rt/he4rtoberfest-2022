4442c272bb6a5ca13629d769274d94ce6ef2bfc2
JesusGautamah <lima.jesuscc@gmail.com>
2022-10-05 19:07:32 -0300

    format output

--- challenges/6-converter-segundos/ruby/jesusgautamah/seconds_to_hours.rb
+++ challenges/6-converter-segundos/ruby/jesusgautamah/seconds_to_hours.rb
@@ -4,10 +4,18 @@
 module SecondsToHours
   class << self
     def convert(seconds)
-      hours = seconds / 3600
-      minutes = (seconds % 3600) / 60
-      seconds = (seconds % 3600) % 60
-      "#{hours}:#{minutes}:#{seconds}"
+      @hours = seconds / 3600
+      @minutes = (seconds % 3600) / 60
+      @seconds = (seconds % 3600) % 60
+      format
+    end
+
+    private
+
+    attr_reader :hours, :minutes, :seconds
+
+    def format
+      "#{hours.to_s.rjust(2, '0')}:#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
     end
   end
 end
