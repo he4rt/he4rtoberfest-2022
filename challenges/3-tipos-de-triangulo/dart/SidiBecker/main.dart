985b362dd65e3b7781fe374ad7d00f88378e2414
Sidi Becker <sidibecker@hotmail.com>
2022-10-05 12:40:44 -0300

    Desafio 3 - SidiBecker

--- challenges/3-tipos-de-triangulo/dart/SidiBecker/main.dart
+++ challenges/3-tipos-de-triangulo/dart/SidiBecker/main.dart
@@ -1,15 +1,6 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  var entrada = arguments.join(', ');
-
-  // Ordena em ordem decrescente
-  arguments.sort((b, a) => a.compareTo(b));
-
-  var pontoA = double.parse(arguments[0]);
-  var pontoB = double.parse(arguments[1]);
-  var pontoC = double.parse(arguments[2]);
-
   var mensagem = '';
 
   // Quando já existe alguma mensagem, quebra a linha para por a próxima.
@@ -21,11 +12,27 @@ void main(List<String> arguments) {
     }
   };
 
+  var entrada = arguments.join(', ');
+
+  // Transforma os argumentos em valores double.
+  var pontos = arguments.map((e) => double.parse(e)).toList();
+
+  // Ordena em ordem decrescente
+  pontos.sort((b, a) => a.compareTo(b));
+
+  var pontoA = pontos[0];
+  var pontoB = pontos[1];
+  var pontoC = pontos[2];
+
   if (pontoA >= (pontoB + pontoC)) {
     print('Entrada: ' + entrada);
     print('NAO FORMA TRIANGULO');
     exit(0);
   }
+
+  pontoA = pontoA * pontoA;
+  pontoB = pontoB * pontoB;
+  pontoC = pontoC * pontoC;
 
   if (pontoA == (pontoB + pontoC)) {
     appendMessage('TRIANGULO RETANGULO');
