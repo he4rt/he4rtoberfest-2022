9298f4db05ddfbb8744ed60f71b8b99702175c2f
emptybrain <franciel.x006@gmail.com>
2022-10-05 20:13:33 -0300

    Update formatting for seconds hours and seconds
    Co-authored-by: Alisson Lima <58952984+alissongdev@users.noreply.github.com>

--- challenges/6-converter-segundos/C/emptybrain/segundos.c
+++ challenges/6-converter-segundos/C/emptybrain/segundos.c
@@ -7,6 +7,6 @@ int main() {
   int hour = seconds / 3600;
   int minutes = (seconds - hour * 3600) / 60;
   seconds = seconds - hour * 3600 - minutes * 60;
-  printf("%d:%02d:%d \n", hour, minutes, seconds);
+  printf("%02d:%02d:%02d \n", hour, minutes, seconds);
   return 0;
 }
