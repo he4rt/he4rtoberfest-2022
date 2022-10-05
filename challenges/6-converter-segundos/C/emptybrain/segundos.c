9d025f642c641b12f094276e964bcd4c351abc06
Franciel <franciel.x006@gmail.com>
2022-10-05 19:51:14 -0300

    add

--- challenges/6-converter-segundos/C/emptybrain/segundos.c
+++ challenges/6-converter-segundos/C/emptybrain/segundos.c
@@ -7,5 +7,6 @@ int main() {
   int hour = seconds / 3600;
   int minutes = (seconds - hour * 3600) / 60;
   seconds = seconds - hour * 3600 - minutes * 60;
-  printf("%d:%d:%d \n", hour, minutes, seconds);
+  printf("%d:%02d:%d \n", hour, minutes, seconds);
+  return 0;
 }
