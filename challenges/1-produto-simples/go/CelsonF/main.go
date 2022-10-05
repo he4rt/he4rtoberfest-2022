475ce34ad7f4b28329f580d46a714155155ae9c6
CelsonF <celsondev@outlook.com>
2022-10-04 23:26:43 -0300

    create branch correct

--- challenges/1-produto-simples/go/CelsonF/main.go
+++ challenges/1-produto-simples/go/CelsonF/main.go
@@ -5,18 +5,5 @@ import (
 )
 
 func main() {
-	var firstNumber uint
-	var secondNumber uint
-	var PROD uint
-
-	fmt.Println("Este programa retorna o produto de 2 números inteiros.")
-	fmt.Println("Por favor digite o 1° número:")
-	fmt.Scan(&firstNumber)
-	fmt.Println("Por favor digite o 2° número:")
-	fmt.Scan(&secondNumber)
-
-	PROD = firstNumber * secondNumber
-
-	fmt.Printf("\nProduto: %v", PROD)
-
+	fmt.Println("Hello my friends , start the chaos")
 }
