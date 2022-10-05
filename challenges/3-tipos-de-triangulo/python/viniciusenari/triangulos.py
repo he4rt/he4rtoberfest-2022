f1af45469e72c10a7bce06aeb6af6e3d3b8abed9
dzou <viniciusenari@gmail.com>
2022-10-05 09:07:47 -0300

    Arrumado problema em que 1 triangulo pode ser de mais de 1 tipo

--- challenges/3-tipos-de-triangulo/python/viniciusenari/triangulos.py
+++ challenges/3-tipos-de-triangulo/python/viniciusenari/triangulos.py
@@ -1,19 +1,22 @@
-def tipo_triangulo(a: float, b: float, c: float) -> str:
+def tipo_triangulo(a: float, b: float, c: float) -> None:
     if a >= b + c:
-        return 'NAO FORMA TRIANGULO'
-    elif a ** 2 == b ** 2 + c ** 2:
-        return 'TRIANGULO RETANGULO'
+        print('NAO FORMA TRIANGULO')
+        return
+
+    if a ** 2 == b ** 2 + c ** 2:
+        print('TRIANGULO RETANGULO')
     elif a ** 2 > b ** 2 + c ** 2:
-        return 'TRIANGULO OBTUSANGULO'
+        print('TRIANGULO OBTUSANGULO')
     elif a ** 2 < b ** 2 + c ** 2:
-        return 'TRIANGULO ACUTANGULO'
-    elif a == b == c:
-        return 'TRIANGULO EQUILATERO'
+        print('TRIANGULO ACUTANGULO')
+    
+    if a == b == c:
+        print('TRIANGULO EQUILATERO')
     elif a == b or c == b:
-        return 'TRIANGULO ISOSCELES'
+        print('TRIANGULO ISOSCELES')
 
 if __name__ == '__main__':
     lados = [float(n) for n in  input().split()]
     lados.sort()
     a, b, c = lados[2], lados[1], lados[0]
-    print(tipo_triangulo(a, b, c))
+    tipo_triangulo(a, b, c)
