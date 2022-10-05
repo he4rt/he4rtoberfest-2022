f3ffe88538ba4b9256f004b755ca630e89f6d098
Michel Wene <michelwene@hotmail.com>
2022-10-04 22:42:53 -0300

    Finalizando desafio 7

---
+++ challenges/7-jokenpo-nerd/javascript/michelwene/7-jokenpoNerd.js
@@ -0,0 +1,44 @@
+const myArgument = process.argv.slice(2);
+
+const numberOfCases = myArgument[0];
+
+const tests = Array.from({ length: numberOfCases }, (_, i) => {
+  const [player1, player2] = myArgument[i + 1].split(" ");
+  return [player1, player2];
+});
+const results = [];
+
+for (let i = 0; i < numberOfCases; i++) {
+  const test = tests[i];
+
+  const player1 = test[0];
+  const player2 = test[1];
+
+  if (player1 === player2) {
+    results.push("empate");
+  } else if (player1 === "tesoura" && player2 === "papel") {
+    results.push("rajesh");
+  } else if (player1 === "papel" && player2 === "pedra") {
+    results.push("rajesh");
+  } else if (player1 === "pedra" && player2 === "lagarto") {
+    results.push("rajesh");
+  } else if (player1 === "lagarto" && player2 === "spock") {
+    results.push("rajesh");
+  } else if (player1 === "spock" && player2 === "tesoura") {
+    results.push("rajesh");
+  } else if (player1 === "tesoura" && player2 === "lagarto") {
+    results.push("rajesh");
+  } else if (player1 === "lagarto" && player2 === "papel") {
+    results.push("rajesh");
+  } else if (player1 === "papel" && player2 === "spock") {
+    results.push("rajesh");
+  } else if (player1 === "spock" && player2 === "pedra") {
+    results.push("rajesh");
+  } else if (player1 === "pedra" && player2 === "tesoura") {
+    results.push("rajesh");
+  } else {
+    results.push("sheldon");
+  }
+}
+
+console.log(results.toString().replace(/,/g, ""));
