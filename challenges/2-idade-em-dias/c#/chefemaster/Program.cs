0414d6ffcb231c3b85e3aab8ebdbd1771f286ea7
Jeferson M Bruno <j.bruno@ga.agr.br>
2022-10-05 14:22:15 -0300

    Finalizando o desafio

---
+++ challenges/2-idade-em-dias/c#/chefemaster/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+  
+class Program {
+  
+    const int DAYS_OF_YEAR = 365;
+    const int DAYS_OF_MONTH = 30;
+    // Main Method
+    static public void Main(String[] args)
+    {
+        if (args.Length <= 0) return;        
+        if (!int.TryParse(args[0], out int input)) return;
+        
+        int years = (int) input / DAYS_OF_YEAR;
+        int months = (int) (input % DAYS_OF_YEAR) / DAYS_OF_MONTH;
+        int days = (int) (input % DAYS_OF_YEAR) % DAYS_OF_MONTH;
+
+        Console.WriteLine($"{years} ano(s)");
+        Console.WriteLine($"{months} mes(s)");
+        Console.WriteLine($"{days} dia(s)");
+    }
+}
