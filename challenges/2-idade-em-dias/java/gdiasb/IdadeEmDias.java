d5dd74a4667b1a13a9bfc148fd4eebbda0b2bac8
Gabriela <diasgbm@gmail.com>
2022-10-05 18:48:57 -0300

    Finalizando o Desafio 2

---
+++ challenges/2-idade-em-dias/java/gdiasb/IdadeEmDias.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class IdadeEmDias {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite sua idade em dias: ");
+    int idadeDias = scanner.nextInt();
+    scanner.close();
+
+    int diasDoAno = 365;
+    int diasDoMes = 30;
+
+    int quantidadeAnos = idadeDias / diasDoAno;
+    int quantidadeMeses = (idadeDias % diasDoAno) / diasDoMes;
+    int quantidadeDias = ((idadeDias % diasDoAno) % diasDoMes);
+
+    System.out.println(quantidadeAnos + " ano(s) " + quantidadeMeses + ", mes(es) e " + quantidadeDias + " dia(s)");
+
+  }
+}
---
+++ challenges/2-idade-em-dias/java/gdiasb/README.md
@@ -0,0 +1,28 @@
+# Submissão de Exercicio
+
+**Exercicio:** 2 - Idade Em Dias
+
+**Nickname:** gdiasb
+
+**Nível Técnico:** - Estudante
+
+**Empresa:**  Nenhuma
+
+**Twitter**: https://twitter.com/gdiasbm
+
+**Dificuldade de Resolução:** Baixa
+
+**Comentários:** Desafio 2 que lê a entrada de um número inteiro de dias e retorna o valor equivalente em anos, meses e dias;
+IDE: Nenhuma (nvim);
+
+**Como rodar o desafio**: 
+
+Certifique-se que o Java está instalado na sua máquina e use o comando abaixo: 
+```bash
+java IdadeEmDias
+```
+Caso queira compilar novamente, digite:
+```bash
+javac IdadeEmDias.java
+java IdadeEmDias
+```
