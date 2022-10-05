526ab70a3937cec5e6264cff97db05024e62523d
Mateus Santos <40031501+Mateussnts@users.noreply.github.com>
2022-10-05 20:50:06 -0300

    Update IdadeEmDias.java

--- challenges/2-idade-em-dias/java/mateussnts/IdadeEmDias.java
+++ challenges/2-idade-em-dias/java/mateussnts/IdadeEmDias.java
@@ -4,12 +4,14 @@ public class IdadeEmDias {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.printf("Digite aqui sua Idade em Dias: ");
         int idade = sc.nextInt();
 
         int anos = idade / 365;
         int meses = (idade % 365) / 30;
         int dias = (idade % 365) % 30;
 
+        System.out.println("\nResultados: ");
         System.out.println(anos + " ano(s)");
         System.out.println(meses + " mes(es)");
         System.out.println(dias + " dia(s)");
