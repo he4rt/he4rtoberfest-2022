68aa07d930900b399116feaec65b09ff15b324e3
Yuri S <yszuszko123@gmail.com>
2022-10-05 23:56:46 -0300

    variavel 'secondNumber' agora é transformada em número também

--- challenges/1-produto-simples/javascript/szuyuri/app.js
+++ challenges/1-produto-simples/javascript/szuyuri/app.js
@@ -8,9 +8,13 @@ const multiply = (x, y) => x * y
 
 consoleInput.forEach((number, index) => {
     number = parseFloat(number)
+    let secondNumber = parseFloat(consoleInput[index + 1])
+
+    if (isNaN(number) || isNaN(secondNumber)) {
+        process.exit(1)
+    }
 
     if (((index + 1) % 2) != 0) {
-        let secondNumber = consoleInput[index + 1]
 
         console.log(
             `A multiplicação entre ${number} e ${secondNumber} é igual a ${multiply(number, secondNumber)}`
