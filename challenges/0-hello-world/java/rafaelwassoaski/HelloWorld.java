19834a7523f889c914e8bdccde3109f679b2ded1
Rafael Wassoaski <meireles200@hotmail.com>
2022-10-05 21:45:49 -0300

    Removendo package

--- challenges/0-hello-world/java/rafaelwassoaski/HelloWorld.java
+++ challenges/0-hello-world/java/rafaelwassoaski/HelloWorld.java
@@ -1,5 +1,3 @@
-package rafaelwassoaski;
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello He4rtoberfest, from the best language with love\n");
