eccc76556c10781469c3aac4e31198fb5496f125
mnia <marcos.ferreira@gft.com>
2022-10-06 11:39:08 -0300

    Desafio finalizado

---
+++ challenges/2-idade-em-dias/c#/MarcosFerreira17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um numero: ");
+        if (!int.TryParse(Console.ReadLine(), out int x)) throw new ArgumentException("parâmetro inválido.");
+        Console.WriteLine(Dias.Calc(x));
+    }
+    public static class Dias
+    {
+        public static string Calc(int qtd)
+        {
+            const int qtdDiasAno = 365;
+            const int qtdDiasMes = 30;
+            int dias, meses, anos;
+
+            anos = qtd / qtdDiasAno;
+            meses = qtd % qtdDiasAno / qtdDiasMes;
+            dias = qtd % qtdDiasAno % qtdDiasMes;
+
+            return $"{anos} ano(s)\n"
+                    + $"{meses} mese(s)\n"
+                    + $"{dias} dia(s)\n";
+        }
+    }
+}
---
+++ challenges/2-idade-em-dias/c#/MarcosFerreira17/README.md
@@ -0,0 +1,22 @@
+# Submissão de Exercicio
+
+**Exercicio:** 2 - Idade em dias
+
+**Nickname:** marcosferreira17
+
+**Nível Técnico:** - Estagiário -
+
+**Empresa:** - GFT Group -
+
+**Twitter**: https://twitter.com/marcosfw7 (opcional)
+
+**Dificuldade de Resolução:** - Baixa -
+
+**Comentários:** Nesse desafio eu achei que ... (Opcional)
+
+**Como rodar o desafio**: 
+
+Use o comando abaixo: 
+```bash
+dotnet run
+```
