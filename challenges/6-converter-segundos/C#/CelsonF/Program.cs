1e663d86a2da54e878ebe8da2c6e0145852f6520
CelsonF <celsondev@outlook.com>
2022-10-06 20:13:24 -0300

    Ajuste desafio 6

--- challenges/6-converter-segundos/C#/CelsonF/Program.cs
+++ challenges/6-converter-segundos/C#/CelsonF/Program.cs
@@ -2,29 +2,29 @@
 
 namespace desafio6
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            double numeroSegundos;
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double numeroSegundos;
 
-            try
-            {
-                Console.WriteLine("Converter Segundos\nDigite em segundos para converter em HH:MM:SS");
-                numeroSegundos = Convert.ToInt32(Console.ReadLine());
+			try
+			{
+				Console.WriteLine("Converter Segundos\nDigite em segundos para converter em HH:MM:SS");
+				numeroSegundos = Convert.ToDouble(Console.ReadLine());
 
-                double totalHora = numeroSegundos / 3600;
-                numeroSegundos %= 3600;
-                double totalMinutos = Math.Floor(numeroSegundos / 60);
+				double totalHora = numeroSegundos / 3600;
+				numeroSegundos %= 3600;
+				double totalMinutos = Math.Floor(numeroSegundos / 60);
 
-                Console.WriteLine("{0}:{1}:{2}", Math.Floor(totalHora), totalMinutos, numeroSegundos % 60);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Erro {ex}");
-            }
+				Console.WriteLine("{0:00}:{1:00}:{2:00}", Math.Floor(totalHora), totalMinutos, numeroSegundos % 60);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Erro {ex}");
+			}
 
-            Console.ReadKey();
-        }
-    }
+			Console.ReadKey();
+		}
+	}
 }
