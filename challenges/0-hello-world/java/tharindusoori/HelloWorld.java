621b5ebca9795d1c4bdab069eaf2169278b40d46
Tharindu Dilshan Sooriyaarachchi <tdilshan2010@gmail.com>
2022-10-06 17:53:43 +0530

    The block indented

--- challenges/0-hello-world/java/tharindusoori/HelloWorld.java
+++ challenges/0-hello-world/java/tharindusoori/HelloWorld.java
@@ -1,6 +1,5 @@
-public class HelloWorld {
-    
-	public static void main(String[] args) {
+public class HelloWorld { 
+    public static void main(String[] args) {
         System.out.print("Hello World");
     }
 }
