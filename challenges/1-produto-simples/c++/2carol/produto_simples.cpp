06a66fb673b15e830826c6f43e27c852381dd5c6
2carol <30879776+2carol@users.noreply.github.com>
2022-10-06 14:00:53 -0300

    Create produto_simples.cpp

---
+++ challenges/1-produto-simples/c++/2carol/produto_simples.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int n1, n2, prod;
+
+  cout<< "Digite o primeiro numero: "<<endl;
+	cin>>n1;
+	cout<< endl << "Digite o segundo numero: "<<endl;
+	cin>>n2;
+	prod = n1 * n2;
+
+	cout<< endl << "O resultado eh: "<< prod <<endl;
+
+};
