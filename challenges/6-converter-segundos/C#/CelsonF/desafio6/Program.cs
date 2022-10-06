feb1b9037b3fbc899fdaf192de2badb320f1e2c8
CelsonF <celsondev@outlook.com>
2022-10-05 22:54:59 -0300

    desafio6 em CSharp

---
+++ challenges/6-converter-segundos/C#/CelsonF/desafio6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace desafio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numeroSegundos;
+
+            try
+            {
+                Console.WriteLine("Converter Segundos\nDigite em segundos para converter em HH:MM:SS");
+                numeroSegundos = Convert.ToInt32(Console.ReadLine());
+
+                double totalHora = numeroSegundos / 3600;
+                numeroSegundos %= 3600;
+                double totalMinutos = Math.Floor(numeroSegundos / 60);
+
+                Console.WriteLine("{0}:{1}:{2}", Math.Floor(totalHora), totalMinutos, numeroSegundos % 60);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro {ex}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
