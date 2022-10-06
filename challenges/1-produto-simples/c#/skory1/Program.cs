811266cfab594845fc796ebe53b8c80afc9206a1
Marcos <marquinhos.dev@gmail.com>
2022-10-06 16:02:14 -0300

    Finalizando o desafio 1

---
+++ challenges/1-produto-simples/c#/skory1/Program.cs
@@ -0,0 +1,20 @@
+﻿while (true)
+{
+    try
+    {
+        Console.WriteLine("Digite o PRIMEIRO número:");
+        int firstNumber = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o SEGUNDO número:");
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        int prod = firstNumber * secondNumber;
+        Console.WriteLine($"produto: {prod}");
+    
+        break;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
