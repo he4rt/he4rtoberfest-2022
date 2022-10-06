1137985f1b83b71fdd495503414cb3e026aa98f5
Sarah Anduca <sarah.anduca@eurekalabs.com.br>
2022-10-06 12:11:57 -0300

    Desafio #7 finalizado

---
+++ challenges/7-jokenpo-nerd/javascript/codinghobbit/README.md
@@ -0,0 +1,23 @@
+# Submissão de Exercicio
+
+**Exercicio:** 7 - Jokenpo Nerd
+
+**Nickname:** codinghobbit
+
+**Nível Técnico:** - Estudante/Junior
+
+**Empresa:** - Nenhuma
+
+**Twitter**: https://twitter.com/sarahanduca
+
+**Dificuldade de Resolução:** - Baixa
+
+**Comentários:**
+
+**Como rodar o desafio**:
+
+Use o comando abaixo:
+
+```bash
+node desafio7.js
+```
---
+++ challenges/7-jokenpo-nerd/javascript/codinghobbit/desafio7.js
@@ -0,0 +1,50 @@
+function jokenpo(player1, player2) {
+  return (player1 == "tesoura" && player2 == "papel") ||
+    (player1 == "tesoura" && player2 == "lagarto") ||
+    (player1 == "papel" && player2 == "pedra") ||
+    (player1 == "papel" && player2 == "spock") ||
+    (player1 == "pedra" && player2 == "tesoura") ||
+    (player1 == "pedra" && player2 == "lagarto") ||
+    (player1 == "lagarto" && player2 == "spock") ||
+    (player1 == "lagarto" && player2 == "papel") ||
+    (player1 == "spock" && player2 == "tesoura") ||
+    (player1 == "spock" && player2 == "pedra")
+    ? "player1"
+    : (player2 == "tesoura" && player1 == "papel") ||
+      (player2 == "tesoura" && player1 == "spock") ||
+      (player2 == "tesoura" && player1 == "lagarto") ||
+      (player2 == "papel" && player1 == "pedra") ||
+      (player2 == "papel" && player1 == "spock") ||
+      (player2 == "pedra" && player1 == "tesoura") ||
+      (player2 == "pedra" && player1 == "lagarto") ||
+      (player2 == "lagarto" && player1 == "spock") ||
+      (player2 == "lagarto" && player1 == "papel") ||
+      (player2 == "spock" && player1 == "tesoura") ||
+      (player2 == "spock" && player1 == "pedra")
+    ? "player2"
+    : "empate";
+}
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question(
+  "Jogue, Ex. 3 spock pedra tesoura tesoura papel lagarto: ",
+  (moves) => {
+    const playersMoves = moves.split(" ").splice(1);
+    playersMoves.forEach((move, player) => {
+      if (player % 2 === 0) {
+        const gameResult = jokenpo(move, playersMoves[player + 1]);
+        gameResult === "player1"
+          ? console.log("rajesh")
+          : gameResult === "player2"
+          ? console.log("sheldon")
+          : console.log("empate");
+      }
+    });
+
+    readline.close();
+  }
+);
