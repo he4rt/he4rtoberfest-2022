df7f3d1010995e9c728336a32c6785c0708ab008
mnia <marcos.ferreira@gft.com>
2022-10-06 10:40:18 -0300

    Finalizando desafio 01

---
+++ challenges/1-produto-simples/c#/marcosferreira17/Challenge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Digite o valor do primeiro produto: ");
+        if (!int.TryParse(Console.ReadLine(), out int x)) throw new ArgumentException("Primeiro parâmetro inválido.");
+
+        Console.Write("Digite o valor do segundo produto: ");
+        if (!int.TryParse(Console.ReadLine(), out int y)) throw new ArgumentException("Segundo parâmetro inválido.");
+        Console.WriteLine(Produto.Calc(x, y));
+    }
+    public static class Produto
+    {
+        public static string Calc(int x, int y)
+        {
+            int result = x * y;
+            return $"Produto: {result}";
+        }
+    }
+}
