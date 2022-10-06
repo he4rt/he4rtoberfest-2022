70243f4e22a653b6adade0747cb30ee02210e610
Rafael Wassoaski <meireles200@hotmail.com>
2022-10-05 21:32:55 -0300

    Finalizando o desafio 0

---
+++ challenges/0-hello-world/java/rafaelwassoaski/HelloWorld.java
@@ -0,0 +1,7 @@
+package rafaelwassoaski;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello He4rtoberfest, from the best language with love\n");
+    }
+}
