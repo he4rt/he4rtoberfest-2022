9318ba36e8d22f96f742dd4d74c5a0dec1e303ce
Yuri S <yszuszko123@gmail.com>
2022-10-05 21:58:51 -0300

    changing branches

--- challenges/0-hello-world/javascript/szuyuri/app.js
+++ challenges/0-hello-world/javascript/szuyuri/app.js
@@ -1,8 +1,8 @@
 const helloWorld = ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd']
 
 let str = ''
-helloWorld.forEach(letter => {
-  str += letter
+helloWorld.forEach(e => {
+  str += e
 })
 
 console.log(str)
