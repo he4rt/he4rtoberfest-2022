c17bd3c0fa5e28b4a61541420f0bc148afc98cf2
Guilherme <guilherme.gandra@premiumads.com.br>
2022-10-07 09:13:15 -0300

    Adicionando input

---
+++ challenges/1-produto-simples/c#/guigandra/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int prod1;
+        int prod2;
+
+        Console.WriteLine("Digite o valor do 1 produto:");
+        prod1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o valor do 2 produto:");
+        prod2 = int.Parse(Console.ReadLine());
+
+        var PROD = prod1 * prod2;
+
+        Console.WriteLine("Produto: " + PROD);
+    }
+}
+
+
