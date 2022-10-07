96e2433e0a6d30106b640aee170e0a466983dd87
Ygor Ismael <74953869+YgorIsmael@users.noreply.github.com>
2022-10-07 09:25:39 -0300

    Update ex6.js

--- challenges/6-converter-segundos/javascript/YgorIsmael/ex6.js
+++ challenges/6-converter-segundos/javascript/YgorIsmael/ex6.js
@@ -1,9 +1,9 @@
 // =D
 const tempo = process.argv[2];
 function conversao(valor){
-    horas = Math.floor(valor/3600).toString().padStart(2, '0');;
-    minutos = Math.floor((valor%3600)/60).toString().padStart(2, '0');;
-    segundos = Math.floor((valor%3600)%60).toString().padStart(2, '0');;
-console.log(`${horas} hora(s) ${minutos} minuto(s) ${segundos} segundo(s)`);
+    horas = Math.floor(valor/3600).toString().padStart(2, '0');
+    minutos = Math.floor((valor%3600)/60).toString().padStart(2, '0');
+    segundos = Math.floor((valor%3600)%60).toString().padStart(2, '0');
+console.log(`${horas}:${minutos}:${segundos}`);
 }
 conversao(tempo);
