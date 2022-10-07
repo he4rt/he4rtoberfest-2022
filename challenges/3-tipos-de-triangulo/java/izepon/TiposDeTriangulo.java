c1e874f063d6ce12a975d6fecc116909b2a5e883
Jean Carlos Izepon <izeponjc@gmail.com>
2022-10-07 15:23:47 -0300

    ajuste

--- challenges/3-tipos-de-triangulo/java/izepon/TiposDeTriangulo.java
+++ challenges/3-tipos-de-triangulo/java/izepon/TiposDeTriangulo.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 import static java.lang.Math.pow;
 
 public class TiposDeTriangulo {
+	
    public static void main(String[] args) {
       start();
    }
@@ -36,24 +37,23 @@ public class TiposDeTriangulo {
          System.out.println("NAO FORMA TRIANGULO");
       } else {
 
-         if(pow(sideA, 2) == pow(sideB, 2) + pow(sideC, 2)){
+         if (pow(sideA, 2) == pow(sideB, 2) + pow(sideC, 2)){
             System.out.println("TRIANGULO RETANGULO");
          }
 
-         if(pow(sideA, 2) > pow(sideB, 2) + pow(sideC, 2)){
+         if (pow(sideA, 2) > pow(sideB, 2) + pow(sideC, 2)){
             System.out.println("TRIANGULO OBTUSANGULO");
          }
 
-         if(pow(sideA, 2) < pow(sideB, 2) + pow(sideC, 2)){
+         if (pow(sideA, 2) < pow(sideB, 2) + pow(sideC, 2)){
             System.out.println("TRIANGULO ACUTANGULO");
          }
 
-         if(sideA == sideB && sideB == sideC){
+         if (sideA == sideB && sideB == sideC){
             System.out.println("TRIANGULO EQUILATERO");
-         }else if(sideA == sideB || sideB == sideC || sideC == sideA){
+         } else if (sideA == sideB || sideB == sideC || sideC == sideA){
             System.out.println("TRIANGULO ISOSCELES");
          }
-
       }
    }
 }
