96d4382fb46cff80a96e33e21296c86767c735f5
Gabriela <70709761+gdiasb@users.noreply.github.com>
2022-10-07 19:46:55 -0300

    Finalizando o Desafio 4

---
+++ challenges/4-fizz-buzz/java/gdiasb/FizzBuzz.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+
+  public static void main(String[] args) {
+  
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite um valor inteiro:");
+    
+    Integer entradaNumero = scanner.nextInt();
+    scanner.close();
+  
+    System.out.println(15%5);
+
+    for (int i = 1; i <= entradaNumero; i++) {
+   
+      if (i % 3 == 0 && i % 5 != 0) {
+        System.out.println("Fizz");
+      }
+      else if (i % 5 == 0 && i % 5 != 0) {
+        System.out.println("Buzz");
+      }
+      else if (i % 3 == 0 && i % 5 == 0) {
+        System.out.println("FizzBuzz");
+      } else {
+        System.out.println(i);
+      }
+    
+    }
+  }
+
+
+}
