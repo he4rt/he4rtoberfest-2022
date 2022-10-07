18ae938df92b313407d8470f39c7e303805697bb
Guilherme <guilherme.gandra@premiumads.com.br>
2022-10-07 07:08:41 -0300

    Finalizando Desafio 1

---
+++ challenges/1-produto-simples/c#/guigandra/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var prod1 = 10;
+        var prod2 = 3;
+        var PROD = prod1 * prod2;
+        Console.WriteLine("Produto: " + PROD);
+
+        var prod3 = 5;
+        var prod4 = 5;
+        var PRODB = prod3 * prod4;
+        Console.WriteLine("Produto: " + PRODB);
+    }
+}
