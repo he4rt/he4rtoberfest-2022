5bfb2fbdcd8f4f7ee634275903e24a4eb964020c
Amorim33 <aluamorim@hotmail.com>
2022-10-07 00:15:31 -0300

    Finalizando desafio

--- challenges/4-fizz-buzz/python/Amorim33/fizz_buzz.py
+++ challenges/4-fizz-buzz/python/Amorim33/fizz_buzz.py
@@ -1,5 +1,14 @@
-n, m = map(int, input().split())
+n = int(input())
 
-PROD = n * m
+numbers = [i for i in range(1, n+1)]
 
-print("produto: ", PROD)
+print("saída:")
+for i in range(len(numbers)):
+    if numbers[i] % 3 == 0 and numbers[i] % 5 == 0:
+        print("FizzBuzz")
+    elif numbers[i] % 3 == 0:
+        print("Fizz")
+    elif numbers[i] % 5 == 0:
+        print("Buzz")
+    else:
+        print(numbers[i])
