d7f41624847b54210466234a731be01f98cbf3ed
Sarah <anduca.sarah@gmail.com>
2022-10-07 08:12:52 -0300

    Adiciona tratamento de número de dados errado e contagem de animais correta

--- challenges/5-cobaias/javascript/codinghobbit/desafio5.js
+++ challenges/5-cobaias/javascript/codinghobbit/desafio5.js
@@ -6,19 +6,28 @@ const readline = require("readline").createInterface({
 readline.question(
   "Insira os dados de pesquisa. Ex '10 10 C 6 R 15 S ...' ",
   (researchData) => {
+    const totalInputTests = researchData.split(" ")[0];
     const data = researchData.split(" ").slice(1);
+    if (data.length !== totalInputTests * 2) {
+      console.log("Insira a quantidade correta de dados");
+      readline.close();
+      return
+    }
     const dataCounters = { total: 0, rabbits: 0, rats: 0, frogs: 0 };
 
     data.forEach((item, index) => {
+      const animal = data[index + 1];
       if (index % 2 === 0) {
-        dataCounters.total += parseInt(item);
-        data[index + 1].toUpperCase() === "C"
+        if(animal.toUpperCase() === 'C' || animal.toUpperCase() === 'R'|| animal.toUpperCase() === 'S'){
+          dataCounters.total += parseInt(item);
+          animal.toUpperCase() === "C"
           ? (dataCounters.rabbits += parseInt(item))
-          : data[index + 1].toUpperCase() === "R"
+          : animal.toUpperCase() === "R"
           ? (dataCounters.rats += parseInt(item))
-          : data[index + 1].toUpperCase() === "S"
+          : animal.toUpperCase() === "S"
           ? (dataCounters.frogs += parseInt(item))
           : null;
+        }
       }
     });
     console.log(`
