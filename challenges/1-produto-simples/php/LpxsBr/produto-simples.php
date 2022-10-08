279823ccc7dafb74f9830394010de9e7cce803b4
Kleber <anselmolopes.an@gmail.com>
2022-10-08 18:53:24 -0300

    Finalizando desafio

--- challenges/1-produto-simples/php/LpxsBr/produto-simples.php
+++ challenges/1-produto-simples/php/LpxsBr/produto-simples.php
@@ -4,5 +4,4 @@ function produto(int $num1, int $num2){
     $PROD = ($num1 * $num2);
     return 'produto: '.($PROD);
 }
-
 echo $resp = (isset($argv[1]) and isset($argv[2])) ? produto($argv[1], $argv[2]) : "valores não informados, tente novamente";
