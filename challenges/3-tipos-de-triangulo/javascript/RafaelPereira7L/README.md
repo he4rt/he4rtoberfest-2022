a4fa05ca9344e49d7634455977bdf67e351d4bdd
Rafael Pereira <rafaelhpmg@gmail.com>
2022-10-08 15:11:02 -0300

    Finalizando o desafio 3

---
+++ challenges/3-tipos-de-triangulo/javascript/RafaelPereira7L/README.md
@@ -0,0 +1,30 @@
+# Submissão de Exercicio
+
+**Exercicio:** 3 - Tipos de triângulo
+
+**Nickname:** RafaelPereira7L
+
+**Nível Técnico:** - Estudante
+
+**Empresa:** - Nenhuma
+
+**Twitter**: https://twitter.com/fsosats
+
+**Dificuldade de Resolução:** - Baixa
+
+
+**Como rodar o desafio**: 
+
+Use o comando abaixo: 
+```bash
+node triangulos.js lado1 lado2 lado3
+```
+Exemplo: 
+```bash
+node triangulos.js 7.0 5.0 7.0
+```
+Output: 
+```bash
+TRIÂNGULO ACUTÂNGULO
+TRIÂNGULO ISÓSCELES
+```
---
+++ challenges/3-tipos-de-triangulo/javascript/RafaelPereira7L/triangulos.js
@@ -0,0 +1,25 @@
+const lado1 = Number(process.argv.slice(2,3)); 
+const lado2 = Number(process.argv.slice(3,4));
+const lado3 = Number(process.argv.slice(4,5));
+
+const verificaTriangulo = (lado1, lado2, lado3) => {
+    if(lado1 <= 0 || lado2 <= 0 || lado3 <= 0) {
+        return console.log('Os lados devem ser maiores que zero');
+    } 
+    if(lado1 >= lado2+lado3 || lado2 >= lado1 + lado3 || lado3 >= lado1 + lado2) {
+        return console.log('NÃO FORMA UM TRIÂNGULO');
+    } else if(lado1**2 == lado2**2 + lado3**2 || lado2**2 == lado1**2 + lado3**2 || lado3**2 == lado1**2 + lado2**2) {
+        console.log('TRIÂNGULO RETÂNGULO');
+    } else if(lado1**2 > lado2**2 + lado3**2 || lado2**2 > lado1**2 + lado3**2 || lado3**2 > lado1**2 + lado2**2) {
+        console.log('TRIÂNGULO OBTUSÂNGULO');
+    } else if(lado1**2 < lado2**2 + lado3**2 || lado2**2 < lado1**2 + lado3**2 || lado3**2 < lado1**2 + lado2**2) {
+        console.log('TRIÂNGULO ACUTÂNGULO');
+    }
+
+    if(lado1 == lado2 && lado2 == lado3) {
+        console.log('TRIÂNGULO EQUILÁTERO');
+    } else if(lado1 == lado2 || lado2 == lado3 || lado1 == lado3) {
+        console.log('TRIÂNGULO ISÓSCELES');
+    }
+}
+verificaTriangulo(lado1, lado2, lado3);
