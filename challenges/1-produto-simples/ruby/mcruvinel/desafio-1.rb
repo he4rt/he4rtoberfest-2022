7eb8a2fe8c044202bfcfb1490d15ea977ae1f468
mcruvinel <matheuscruvinel@hotmail.com>
2022-10-07 23:14:45 -0300

    Desafio 1 FInalizado

---
+++ challenges/1-produto-simples/ruby/mcruvinel/desafio-1.rb
@@ -0,0 +1,6 @@
+puts "Digite o primeiro número em que deseja o produto:"
+num1 = gets.chomp.to_i
+puts "Digite o segundo número em que deseja o produto:"
+num2 = gets.chomp.to_i
+prod = num1 * num2
+puts "Produto == #{prod}"
