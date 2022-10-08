4faf04654a8fb2a83167f6eeef95cf7ab5deb32c
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2022-10-08 19:19:13 -0300

    Altera retorno da função calculateProduct

--- challenges/1-produto-simples/javascript/Raikouve/index.js
+++ challenges/1-produto-simples/javascript/Raikouve/index.js
@@ -1,5 +1,5 @@
 function calculateProduct(num1, num2) {
-  return `produto: ${num1 * num2}`;
+  return num1 * num2;
 }
 
-console.log(calculateProduct(2, 3));
+console.log(`produto: ${calculateProduct(2, 3)}`);
