8404efacba9fcd04c3a72c0de14718243f501dc5
Gabriel Grubba <Grubba27@hotmail.com>
2022-10-07 22:45:32 -0300

    feat: adjusts by luiz

--- challenges/1-produto-simples/rust/grubba27/src/main.rs
+++ challenges/1-produto-simples/rust/grubba27/src/main.rs
@@ -3,12 +3,12 @@ use std::num::ParseIntError;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (n1, n2) = parse_args(&args);
+    let (n1, n2) = parse_args(&args).expect("Errow");
     let result = n1 * n2;
     println!("produto: {}", result);
 }
-fn parse_args(args: &[String]) -> Result<(i64, i64), ParseIntError()> {
-    let n1: i64 = (&args[1]).parse().expect("errow o 1");
-    let n2: i64 = (&args[2]).parse().expect("errow o 2");
+fn parse_args(args: &[String]) -> Result<(i64, i64), ParseIntError> {
+    let n1: i64 = (&args[1]).parse()?;
+    let n2: i64 = (&args[2]).parse()?;
     Ok((n1, n2))
 }
