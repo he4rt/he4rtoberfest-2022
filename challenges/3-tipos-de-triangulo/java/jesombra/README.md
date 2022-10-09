bd364a3c27755ad15abf1f826f14dfffe43e15ea
Jefferson Sombra <soombra73@gmail.com>
2022-10-09 00:05:19 -0300

    Finalizando o desafio

---
+++ challenges/3-tipos-de-triangulo/java/jesombra/README.md
@@ -0,0 +1,27 @@
+# Submissão de Exercicio
+
+**Exercicio:** 3 - Tipos de Triângulo
+
+**Nickname:** jesombra
+
+**Nível Técnico:** - Estudante/Junior
+
+**Empresa:** - Nenhuma
+
+**Twitter**: https://twitter.com/jesombra
+
+**Dificuldade de Resolução:** - Média
+
+**Comentários:** Classifiquei commo médio, pois tive que pesquisar em alguns lugares para conseguir ordenar um array decrescente como solicitado no exercicio, mas encontrei.
+
+**Como rodar o desafio**: 
+
+Use o comando abaixo compilar: 
+```
+javac TiposDeTriangulo.java
+```
+
+Use o comando abaixo rodar: 
+```
+java TiposDeTriangulo
+```
---
+++ challenges/3-tipos-de-triangulo/java/jesombra/TiposDeTriangulo.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TiposDeTriangulo{
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Dados: ");
+        Double[] ladosTriangulo = {sc.nextDouble(), sc.nextDouble(), sc.nextDouble()};
+        
+        Arrays.sort(ladosTriangulo, Collections.reverseOrder());
+        System.out.println();
+        System.out.print("Entrada: ");
+        for(int i = 0; i < ladosTriangulo.length; i++){
+            System.out.print(ladosTriangulo[i]+ " ");
+        }
+        System.out.println();
+        System.out.println();
+        checarTriangulo(ladosTriangulo);
+        
+
+        sc.close();
+        }
+
+        public static void checarTriangulo(Double[] ladosTriangulo){
+
+            double ladoA = ladosTriangulo[0];
+            double ladoB = ladosTriangulo[1];
+            double ladoC = ladosTriangulo[2];
+
+            if(ladoA >= (ladoB + ladoC)){
+                System.out.println("Não forma Triangulo");
+            } else {
+                if (Math.pow(ladoA, 2) == Math.pow(ladoB, 2) + Math.pow(ladoC, 2)){
+                    System.out.println("Triangulo Retangulo");
+                } 
+                if (Math.pow(ladoA, 2) > Math.pow(ladoB, 2) + Math.pow(ladoC, 2)){
+                    System.out.println("Triangulo Obtusangulo");
+                } 
+                if (Math.pow(ladoA, 2) < Math.pow(ladoB, 2) + Math.pow(ladoC, 2)){
+                    System.out.println("Triangulo Acutangulo");
+                }
+                if (ladoA == ladoB && ladoB == ladoC){
+                    System.out.println("Triangulo Equilatero");
+                } else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC){
+                    System.out.println("Triangulo Isoceles");
+                }
+        }
+    }
+}
