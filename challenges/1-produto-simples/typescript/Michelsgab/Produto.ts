b2058bbf2f7c9ab74262a45355a89cbe01a64244
Michelsgab <eugabmichels@gmail.com>
2022-10-10 10:11:39 -0300

    Desafio 1 finalizado - ajuste pedido por paulohgs

--- challenges/1-produto-simples/typescript/Michelsgab/Produto.ts
+++ challenges/1-produto-simples/typescript/Michelsgab/Produto.ts
@@ -1,6 +1,12 @@
-function somaProduto(numero1: number, numero2:number) {
-    let resultado = numero1 * numero2;
-    return resultado
+function somaProduto(numero1: number, numero2: number) {
+  let resultado = numero1 * numero2;
+  return console.log(`O valor do produto é de ${resultado}`);
 }
 
-console.log(somaProduto(2, 5));
+function recebeValores() {
+  let valor1 = Number(prompt("Insira o primeiro valor"));
+  let valor2 = Number(prompt("Insira o segundo valor"));
+  return somaProduto(valor1, valor2);
+}
+
+recebeValores()
