3b1f68af55fe2b369fecc5362093aef1497638a1
Willian Magnum Albeche <willianalbeche@gmail.com>
2022-10-10 15:12:54 -0300

    Finalizando desafio

---
+++ challenges/6-converter-segundos/swift/WillianAlbeche/converter-em-segundos.swift
@@ -0,0 +1,12 @@
+print("Por favor, escreva o input: ")
+let inputSeconds = Int(readLine()!)
+var hour: Int = inputSeconds! / 3600
+var minute: Int = Int((inputSeconds! % 3600) / 60)
+var seconds: Int = Int((inputSeconds! % 3600) % 60)
+
+var result: String = ""
+result += hour < 10 ? "0\(hour)" : "\(hour)"
+result += minute < 10 ? ":0\(minute)" : ":\(minute)"
+result += seconds < 10 ? ":0\(seconds)" : ":\(seconds)"
+
+print("Resultado: \(result)")
