406264c4740160e3ce508ddf19cc97bfb379a587
maikella <fi4przac@anonaddy.me>
2022-10-10 16:36:33 -0300

    desafio 6 finalizado

---
+++ challenges/6-converter-segundos/java/maikella/ConverterSegundos.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ConverterSegundos {
+
+   public static void main(String[] args) {
+
+      converterSegundos();
+
+   }
+
+   private static void converterSegundos(){
+
+      Scanner s = new Scanner(System.in);
+
+      System.out.println("Digite o valor de segundos: ");
+      int totalDeSegundos = s.nextInt();
+
+      final int HORAS_EM_SEGUNDOS = 3600;
+      final int MINUTOS_EM_SEGUNDOS = 60;
+
+      int quantidadeHoras = totalDeSegundos / HORAS_EM_SEGUNDOS;
+      int quantidadeMinutos = (totalDeSegundos % HORAS_EM_SEGUNDOS) /MINUTOS_EM_SEGUNDOS;
+      int quantidadeSegundos = (totalDeSegundos % HORAS_EM_SEGUNDOS) % MINUTOS_EM_SEGUNDOS;
+
+      System.out.printf("%02d:%02d:%02d", quantidadeHoras, quantidadeMinutos, quantidadeSegundos);
+
+   }
+}
