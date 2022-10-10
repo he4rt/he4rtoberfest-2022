7e276c43a85a21070f536ae0de13103aa8ff7d9a
Jean Carlos Izepon <izeponjc@gmail.com>
2022-10-10 17:31:52 -0300

    ajuste indentação

--- challenges/4-fizz-buzz/java/izepon/FizzBuzz.java
+++ challenges/4-fizz-buzz/java/izepon/FizzBuzz.java
@@ -3,24 +3,24 @@ import java.util.Scanner;
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		
-	   Scanner s = new Scanner(System.in);
 
-	      System.out.println("Digite aqui o valor: ");
-	      
-	      int num = s.nextInt();
+		Scanner s = new Scanner(System.in);
 
-	      for (int i = 1; i <= num; i++) {
+		System.out.println("Digite aqui o valor: ");
 
-	         if(i % 3 == 0 && i % 5 == 0){
-	            System.out.println("FizzBuzz");
-	         }else if(i % 3 == 0){
-	            System.out.println("Fizz");
-	         }else if(i % 5 == 0){
-	            System.out.println("Buzz");
-	         }else {
-	            System.out.println(i);
-	         }
-	      }
-   }
+		int num = s.nextInt();
+
+		for (int i = 1; i <= num; i++) {
+
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (i % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (i % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
 }
