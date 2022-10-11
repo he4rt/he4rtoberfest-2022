a91d35bcac3658e81f61f1b9d30c70e9591755bb
LpxsBr <anselmolopes.an@gmail.com>
2022-10-11 13:44:20 -0300

    finalizando desafio 1

--- challenges/1-produto-simples/php/LpxsBr/produto-simples.php
+++ challenges/1-produto-simples/php/LpxsBr/produto-simples.php
@@ -3,7 +3,7 @@
 if (isset($argv[1]) and isset($argv[2])) {
     $num1 = $argv[1];
     $num2 = $argv[2];
-    echo $PROD = $num1 * $num2;
+    echo 'produto: '.$PROD = $num1 * $num2;
 } else {
     echo 'sem valores informados, tente novamente';
 }
