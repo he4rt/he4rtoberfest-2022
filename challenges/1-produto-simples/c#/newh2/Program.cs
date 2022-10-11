935770b3f336f59cb92687049b41206112056630
Guilherme Xavier <gaxfaria@gmail.com>
2022-10-11 19:04:05 -0300

    Finalizando desafio 1

---
+++ challenges/1-produto-simples/c#/newh2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Digite o valor o primeiro valor:");
+int primieroProduto = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor o primeiro valor:");
+int segundoProduto = int.Parse(Console.ReadLine());
+
+int prod =  primieroProduto * segundoProduto;
+
+Console.WriteLine("Produto: {0}", prod);
+
---
+++ challenges/1-produto-simples/c#/newh2/model.md
@@ -0,0 +1,22 @@
+# Submissão de Exercicio
+
+**Exercicio:** 1 - Produto Simples
+
+**Nickname:** newh2
+
+**Nível Técnico:** - Senior -
+
+**Empresa:** - Funcional HealthTech
+
+**Twitter**: https://twitter.com/GuiXavierFaria 
+
+**Dificuldade de Resolução:** - Baixa
+
+**Comentários:** 
+
+**Como rodar o desafio**: 
+
+Use o comando abaixo: 
+```bash
+dotnet run
+```
