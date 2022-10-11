d9c09a8dce149c6b8781635b8c670fc361e90929
alvarod <alvarod@lacunasoftware.com>
2022-10-10 23:34:25 -0300

    Finalizando o desafio

---
+++ challenges/1-produto-simples/c#/alvarofilho/Program.cs
@@ -0,0 +1,8 @@
+﻿var arg1 = Environment.GetCommandLineArgs()[1];
+var arg2 = Environment.GetCommandLineArgs()[2];
+
+if (int.TryParse(arg1, out var number1) && int.TryParse(arg2, out var number2))
+{
+    Console.WriteLine($"produto: {number1 * number2}");
+}
+
