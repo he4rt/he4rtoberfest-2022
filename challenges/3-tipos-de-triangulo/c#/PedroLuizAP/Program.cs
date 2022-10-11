ef84d413f45be838c163ec77f64fedf549f73760
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-10-11 17:03:24 -0300

    Finalizando o desafio

---
+++ challenges/3-tipos-de-triangulo/c#/PedroLuizAP/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static public void Main(String[] args)
+    {
+        double ladoA, ladoB, ladoC;
+        List<double> lados = new();
+
+        if (double.TryParse(args[0], out ladoA) && double.TryParse(args[1], out ladoB) && double.TryParse(args[2], out ladoC))
+        {
+            lados = new() { ladoA, ladoB, ladoC };
+            lados = lados.OrderByDescending(l => l).ToList();
+
+            if (lados[0] >= lados[1] + lados[2])
+            {
+                Console.WriteLine("NAO FORMA TRIANGULO");
+                return;
+            }
+
+            if (Math.Pow(lados[0], 2) == Math.Pow(lados[1], 2) + Math.Pow(lados[2], 2)) Console.WriteLine("TRIANGULO RETANGULO");
+
+            if (Math.Pow(lados[0], 2) > Math.Pow(lados[1], 2) + Math.Pow(lados[2], 2)) Console.WriteLine("TRIANGULO OBTUSANGULO");
+
+            if (Math.Pow(lados[0], 2) < Math.Pow(lados[1], 2) + Math.Pow(lados[2], 2)) Console.WriteLine("TRIANGULO ACUTANGULO");
+
+            if (lados[0] == lados[1] && lados[1] == lados[2]) Console.WriteLine("TRIANGULO EQUILATERO");
+            else if (lados[0] == lados[1] || lados[1] == lados[2] || lados[0] == lados[2]) Console.WriteLine("TRIANGULO ISOSCELES");
+        }
+    }
+}
---
+++ challenges/3-tipos-de-triangulo/c#/PedroLuizAP/readme.md
@@ -0,0 +1,31 @@
+# Submissão de Exercicio
+
+**Exercicio:** 3 - Tipos de Triângulo
+
+**Nickname:** PedroLuizAP
+
+**Nível Técnico:** Junior
+
+**Empresa:** Agger Sistemas
+
+**Twitter**: https://twitter.com/PedroLuizAP
+
+**Dificuldade de Resolução:**: - Baixa
+
+**Comentários:**: Simples, porem confesso que não lembrava de Cabeça o Math.Pow
+
+**Como rodar o desafio**: 
+
+Use o comando abaixo: 
+```bash
+dotnet run 7.0 5.0 7.0
+```
+```bash
+dotnet run 6.0 6.0 10.0
+```
+```bash
+dotnet run 6.0 6.0 6.0
+```
+```bash
+dotnet run 5.0 7.0 2.0
+```
