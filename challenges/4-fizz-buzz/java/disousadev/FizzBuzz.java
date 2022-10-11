989bcc89409f7a2650df217ea5c37fa058405399
Eder Diego de Sousa <me@diego.dev.br>
2022-10-10 23:10:06 -0300

    Enviando desafio 4

---
+++ challenges/4-fizz-buzz/java/disousadev/FizzBuzz.java
@@ -0,0 +1,13 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Entrada: ");
+        int value = sc.nextInt();
+
+        IntStream.rangeClosed(1, value)
+                .mapToObj(i -> i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz" : "Fizz") : (i % 5 == 0 ? "Buzz" : i))
+                .forEach(System.out::println);
+    }
+}
