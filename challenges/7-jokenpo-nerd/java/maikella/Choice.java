d732820075b112336bfef03fcc0b6ce483342276
maikella <fi4przac@anonaddy.me>
2022-10-11 00:50:42 -0300

    desafio 7 finalizado

---
+++ challenges/7-jokenpo-nerd/java/maikella/Choice.java
@@ -0,0 +1,47 @@
+package com.maikella.exer7;
+
+public enum Choice {
+
+   PAPEL("papel") {
+      @Override
+      public boolean vence(Choice choice) {
+         return choice == Choice.PEDRA || choice == Choice.SPOCK;
+      }
+   },
+   PEDRA("pedra") {
+      @Override
+      public boolean vence(Choice choice) {
+         return choice == Choice.LAGARTO || choice == Choice.TESOURA;
+      }
+   },
+   LAGARTO("lagarto") {
+      @Override
+      public boolean vence(Choice choice) {
+         return choice == Choice.SPOCK || choice == Choice.PAPEL;
+      }
+   },
+   SPOCK("spock") {
+      @Override
+      public boolean vence(Choice choice) {
+         return choice == Choice.TESOURA || choice == Choice.PEDRA;
+      }
+   },
+   TESOURA("tesoura") {
+      @Override
+      public boolean vence(Choice choice) {
+         return choice == Choice.PAPEL || choice == Choice.LAGARTO;
+      }
+   };
+
+   private final String choice;
+
+   Choice(String choice) {
+      this.choice = choice;
+   }
+
+   public String getChoice() {
+      return choice;
+   }
+
+   public abstract boolean vence(Choice choice);
+}
---
+++ challenges/7-jokenpo-nerd/java/maikella/MainJokenpoNerd.java
@@ -0,0 +1,10 @@
+package com.maikella.exer7;
+
+public class MainJokenpoNerd {
+
+   public static void main(String[] args) {
+
+      JokenpoNerd jokenpoNerd = new JokenpoNerd();
+      jokenpoNerd.start();
+   }
+}
