01bdb99bf272ebd7ad94324d5c0898ad84d92e53
alvarod <alvarod@lacunasoftware.com>
2022-10-11 00:03:45 -0300

    Finalizando o desafio

---
+++ challenges/2-idade-em-dias/c#/alvarofilho/Program.cs
@@ -0,0 +1,12 @@
+﻿var args1 = Environment.GetCommandLineArgs()[1];
+
+if (!int.TryParse(args1, out var ageInDays)) return;
+
+const int daysOfYear = 365;
+const int daysOfMonth = 30;
+
+var years = ageInDays / daysOfYear;
+var months = (ageInDays % daysOfYear) / daysOfMonth;
+var days = (ageInDays % daysOfYear) % daysOfMonth;
+
+Console.WriteLine($"{years} ano(s)\n{months} mes(es)\n{days} dia(s)");
