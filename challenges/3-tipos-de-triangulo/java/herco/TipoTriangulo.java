1dfad42489ef04dbb06ac8ff9adc6dc47ac63b3c
Gabriel Pereira <gabrielhpereira518@gmail.com>
2022-10-12 10:14:03 +0000

    Correcao, foi feito push de versao antiga

--- challenges/3-tipos-de-triangulo/java/herco/TipoTriangulo.java
+++ challenges/3-tipos-de-triangulo/java/herco/TipoTriangulo.java
@@ -1,22 +1,28 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class TipoTriangulo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<double> ladosTriangulo = new ArrayList<>();
-        System.out.println("Entre com 3 lados de um triangulo");
-        ladosTriangulo.add(scanner.nextDouble());
-        ladosTriangulo.add(scanner.nextDouble());
-        ladosTriangulo.add(scanner.nextDouble());
+        List<Double> ladosTriangulo = new ArrayList<>();
 
+        System.out.println("Entre com 3 lados de um triangulo");
+
+        ladosTriangulo.add(scanner.nextDouble());
+        ladosTriangulo.add(scanner.nextDouble());
+        ladosTriangulo.add(scanner.nextDouble());
         scanner.close();
 
         System.out.println("");
 
-        validaTriangulo(ladoA,ladoB,ladoC);
+        ladosTriangulo.sort(Comparator.reverseOrder());
+
+        validaTriangulo(ladosTriangulo.get(0), ladosTriangulo.get(1), ladosTriangulo.get(2));
     }
 
-    private static void validaTriangulo(double ladoA, double ladoB, double ladoC) {
+    private static void validaTriangulo(Double ladoA, Double ladoB, Double ladoC) {
         final int expoente = 2;
         final double somaRaizLadoBLadoC = Math.pow(ladoB, expoente) + Math.pow(ladoC, expoente);
         final double raizLadoA = Math.pow(ladoA, expoente);
@@ -28,8 +34,8 @@ public class TipoTriangulo {
             if (raizLadoA == somaRaizLadoBLadoC) System.out.println("TRIANGULO RETANGULO");
             if (raizLadoA > somaRaizLadoBLadoC) System.out.println("TRIANGULO OBTUSANGULO");
             if (raizLadoA < somaRaizLadoBLadoC) System.out.println("TRIANGULO ACUTANGULO");
-            if (ladoA == ladoB && ladoA == ladoC)  System.out.println("TRIANGULO EQUILATERO");
-            else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC) System.out.println("TRIANGULO ISOSCELES");
+            if (ladoA.equals(ladoB) && ladoA.equals(ladoC))  System.out.println("TRIANGULO EQUILATERO");
+            else if (ladoA.equals(ladoB) || ladoA.equals(ladoC) || ladoB.equals(ladoC)) System.out.println("TRIANGULO ISOSCELES");
         }
     }
 }
