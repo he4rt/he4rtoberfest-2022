d49bf3669d452b4586336b3686e5c8a12a732b56
Gabriel Pereira <gabrielhpereira518@gmail.com>
2022-10-12 10:11:07 +0000

    Adicionado lista na entrada de dados para ordenarls

--- challenges/3-tipos-de-triangulo/java/herco/TipoTriangulo.java
+++ challenges/3-tipos-de-triangulo/java/herco/TipoTriangulo.java
@@ -3,16 +3,12 @@ import java.util.Scanner;
 public class TipoTriangulo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        List<double> ladosTriangulo = new ArrayList<>();
         System.out.println("Entre com 3 lados de um triangulo");
-        System.out.print("Lado A: ");
-        double ladoA = scanner.nextDouble();
+        ladosTriangulo.add(scanner.nextDouble());
+        ladosTriangulo.add(scanner.nextDouble());
+        ladosTriangulo.add(scanner.nextDouble());
 
-        System.out.print("Lado B: ");
-        double ladoB = scanner.nextDouble();
-
-        System.out.print("Lado C: ");
-        double ladoC = scanner.nextDouble();
         scanner.close();
 
         System.out.println("");
@@ -25,13 +21,15 @@ public class TipoTriangulo {
         final double somaRaizLadoBLadoC = Math.pow(ladoB, expoente) + Math.pow(ladoC, expoente);
         final double raizLadoA = Math.pow(ladoA, expoente);
 
-        if (ladoA >= ladoB + ladoC) System.out.println("NAO FORMA TRIANGULO");
-            else {
-                if (raizLadoA == somaRaizLadoBLadoC) System.out.println("TRIANGULO RETANGULO");
-                if (raizLadoA > somaRaizLadoBLadoC) System.out.println("TRIANGULO OBTUSANGULO");
-                if (raizLadoA < somaRaizLadoBLadoC) System.out.println("TRIANGULO ACUTANGULO");
-                if (ladoA == ladoB && ladoA == ladoC)  System.out.println("TRIANGULO EQUILATERO");
-                else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC) System.out.println("TRIANGULO ISOSCELES");
+        System.out.println("Entrada: " + ladoC + " " + ladoB + " " + ladoA);
+
+        if (ladoA >= (ladoB + ladoC)) { System.out.println("NAO FORMA TRIANGULO"); }
+        else {
+            if (raizLadoA == somaRaizLadoBLadoC) System.out.println("TRIANGULO RETANGULO");
+            if (raizLadoA > somaRaizLadoBLadoC) System.out.println("TRIANGULO OBTUSANGULO");
+            if (raizLadoA < somaRaizLadoBLadoC) System.out.println("TRIANGULO ACUTANGULO");
+            if (ladoA == ladoB && ladoA == ladoC)  System.out.println("TRIANGULO EQUILATERO");
+            else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC) System.out.println("TRIANGULO ISOSCELES");
         }
     }
 }
