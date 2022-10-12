f987bda21d1c95423beb8726bb4a7a98bdb542b3
Jéssica Souza <>
2022-10-12 20:57:12 -0300

    add readme & refactor

--- challenges/6-converter-segundos/go/jessicamosouza/main.go
+++ challenges/6-converter-segundos/go/jessicamosouza/main.go
@@ -2,26 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 )
 
 func ConvSegundos(s int) string {
-	SToH := s % 3600
 	horas := s / 3600
-	minutos := SToH / 60
-	segundos := SToH % 60
+	minutos := s % 3600 / 60
+	segundos := s % 3600 % 60
 
-	horasString := strconv.Itoa(horas)
-	minutosString := strconv.Itoa(minutos)
-	segundosString := strconv.Itoa(segundos)
+	sHora := fmt.Sprintf("%02d", horas)
+	sMinutos := fmt.Sprintf("%02d", minutos)
+	sSegundos := fmt.Sprintf("%02d", segundos)
 
-	r := horasString + ":" + minutosString + ":" + segundosString
-
-	fmt.Println(r)
-
-	return r
+	return sHora + ":" + sMinutos + ":" + sSegundos
 }
 
 func main() {
-	ConvSegundos(7650)
+	segundos, err := strconv.Atoi(os.Args[1])
+	if err != nil || segundos <= 0 {
+		fmt.Println("Erro no input.")
+		os.Exit(1)
+	}
+
+	fmt.Println(ConvSegundos(segundos))
 }
