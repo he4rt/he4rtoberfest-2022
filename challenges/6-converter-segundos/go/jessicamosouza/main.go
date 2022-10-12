5b9cbb04f3ec176d35b759ebb9bcc501f6ee6c35
Jéssica Souza <>
2022-10-12 20:38:40 -0300

    metade finalizado

--- challenges/6-converter-segundos/go/jessicamosouza/main.go
+++ challenges/6-converter-segundos/go/jessicamosouza/main.go
@@ -1,5 +1,27 @@
 package main
 
-func ConvSegundos(segundos int) string {
-	return "02:07:30"
+import (
+	"fmt"
+	"strconv"
+)
+
+func ConvSegundos(s int) string {
+	SToH := s % 3600
+	horas := s / 3600
+	minutos := SToH / 60
+	segundos := SToH % 60
+
+	horasString := strconv.Itoa(horas)
+	minutosString := strconv.Itoa(minutos)
+	segundosString := strconv.Itoa(segundos)
+
+	r := horasString + ":" + minutosString + ":" + segundosString
+
+	fmt.Println(r)
+
+	return r
+}
+
+func main() {
+	ConvSegundos(7650)
 }
