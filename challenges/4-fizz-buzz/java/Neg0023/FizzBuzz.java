584159b3c5233a7d6be5ee7e0f5bac61b10d0923
Uemerson <uemerson161@hotmail.com>
2022-10-12 19:19:49 -0300

    Finalizando o desafio 4

---
+++ challenges/4-fizz-buzz/java/Neg0023/FizzBuzz.java
@@ -0,0 +1,20 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+
+        System.out.println("saída:");
+
+        for (int i = 1; i <= n; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
---
+++ challenges/4-fizz-buzz/java/Neg0023/README.md
@@ -0,0 +1,23 @@
+# Submissão de Exercicio
+
+**Exercicio:** 4 - Fizz Buzz
+
+**Nickname:** Neg0023
+
+**Nível Técnico:** - Estudante/Estagiário/Junior/Pleno/Senior -
+
+**Empresa:** - Sua Empresa / Nenhuma -
+
+**Twitter**: https://twitter.com/Neg1U
+
+**Dificuldade de Resolução:** - Baixa -
+
+**Comentários:** 
+
+**Como rodar o desafio**: 
+
+Use o comando abaixo: 
+```bash
+javac FizzBuzz.java 
+java FizzBuzz.java arg
+```
