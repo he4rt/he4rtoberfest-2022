2e56909450005ca319fcd656f3358ad5d6acf899
Gabriel Pereira <gabrielhpereira518@gmail.com>
2022-10-12 12:59:18 +0000

    Finalizando desafio 5-Cobaia

---
+++ challenges/5-cobaias/java/herco/Cobaia.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Cobaia {
+    static final Character COELHO = 'C';
+    static final Character RATO = 'R';
+    static final Character SAPO = 'S';
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> casosTeste = new ArrayList<>();
+        List<Character> listaTipoCobaias = new ArrayList<>();
+        List<Integer> listaQuantidadeCobaias = new ArrayList<>();
+
+        System.out.println("Quantos testes foram realizados? ");
+        int totalCasosTeste = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 1; i <= totalCasosTeste; i++) {
+            System.out.print("Teste " + i + ": ");
+            String teste = scanner.nextLine().toUpperCase();
+
+            String[] testeSeparadoEmTotalCobaisETipoCobaia = teste.split(" ");
+            listaTipoCobaias.add(testeSeparadoEmTotalCobaisETipoCobaia[1].charAt(0));
+            listaQuantidadeCobaias.add(Integer.valueOf(testeSeparadoEmTotalCobaisETipoCobaia[0]));
+
+        }
+        scanner.close();
+
+        Integer totalCobaias = 0;
+        for (Integer cobaias : listaQuantidadeCobaias) {
+            totalCobaias += cobaias;
+        }
+        System.out.println("\nTotal: " + totalCobaias + " Cobaias");
+        System.out.println("Total de coelhos: " + calcuaTotalPorTipoCobaia(listaTipoCobaias, listaQuantidadeCobaias, totalCobaias, COELHO));
+        System.out.println("Total de ratos: " + calcuaTotalPorTipoCobaia(listaTipoCobaias, listaQuantidadeCobaias, totalCobaias, RATO));
+        System.out.println("Total de sapos: " + calcuaTotalPorTipoCobaia(listaTipoCobaias, listaQuantidadeCobaias, totalCobaias, SAPO));
+        System.out.printf("Percentual de coelhos:  %.2f%%\n", calculaPorcentagem(listaTipoCobaias, listaQuantidadeCobaias, totalCobaias, COELHO));
+        System.out.printf("Percentual de ratos:  %.2f%%\n", calculaPorcentagem(listaTipoCobaias, listaQuantidadeCobaias, totalCobaias, RATO));
+        System.out.printf("Percentual de sapos: %.2f%%\n", calculaPorcentagem(listaTipoCobaias, listaQuantidadeCobaias, totalCobaias, SAPO));
+    }
+
+    private static int calcuaTotalPorTipoCobaia(List<Character> listaTipoCobaias, List<Integer> listaQuantidadeCobaias, Integer totalCobaias, Character tipoCobaia) {
+        int value = 0;
+        for (int i = 0; i < listaTipoCobaias.size(); i++) {
+            Character cobaia = listaTipoCobaias.get(i);
+            if (cobaia.equals(tipoCobaia)) {
+                value += listaQuantidadeCobaias.get(i);
+            }
+        }
+        return value;
+    }
+
+    private static double calculaPorcentagem(List<Character> listaTipoCobaias, List<Integer> listaQuantidadeCobaias, Integer totalCobaias, Character tipoCobaia) {
+        double value = calcuaTotalPorTipoCobaia(listaTipoCobaias, listaQuantidadeCobaias, totalCobaias, tipoCobaia);
+        return (value * 100) / totalCobaias;
+    }
+}
