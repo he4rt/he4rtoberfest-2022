19e4904a5a8f44f158e6e728c8b3873e6dd9eff9
gushecs <gustavo.heitor@outlook.com>
2022-10-12 17:40:05 -0300

    São aceitos apenas valores inteiros

--- challenges/1-produto-simples/java/gushecs/ProdutoSimples.java
+++ challenges/1-produto-simples/java/gushecs/ProdutoSimples.java
@@ -8,20 +8,20 @@ public class ProdutoSimples {
 
         Scanner scanner = new Scanner(System.in);
         boolean inputLoopingVar = true;
-        double value1 = 0.0;
-        double value2 = 0.0;
+        int value1 = 0;
+        int value2 = 0;
 
-        System.out.println("Este programa irá lhe pedir dois valores e devolver o produto entre eles.");
+        System.out.println("Este programa irá lhe pedir dois valores inteiros e devolver o produto entre eles.");
         System.out.println();
 
         while (inputLoopingVar) {
             try {
                 System.out.print("Digite o primeiro valor do produto: ");
-                value1 = scanner.nextDouble();
+                value1 = scanner.nextInt();
                 inputLoopingVar = false;
             } catch (InputMismatchException e) {
                 System.out.println();
-                System.out.println("Valor inválido! o valor fornecido deve ser numérico.");
+                System.out.println("Valor inválido! o valor fornecido deve ser um número inteiro.");
                 scanner.next();
             }
         }
@@ -31,18 +31,17 @@ public class ProdutoSimples {
         while (inputLoopingVar) {
             try {
                 System.out.print("Digite o segundo valor do produto: ");
-                value2 = scanner.nextDouble();
+                value2 = scanner.nextInt();
                 inputLoopingVar = false;
             } catch (InputMismatchException e) {
                 System.out.println();
-                System.out.println("Valor inválido! o valor fornecido deve ser numérico.");
+                System.out.println("Valor inválido! o valor fornecido deve ser um número inteiro.");
                 scanner.next();
             }
         }
 
         System.out.println();
-        DecimalFormat format = new DecimalFormat("0.#");
-        System.out.println("produto: " + format.format(value1 * value2));
+        System.out.println("produto: " + value1 * value2);
 
     }
 
