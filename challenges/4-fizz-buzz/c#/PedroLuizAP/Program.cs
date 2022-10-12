69e7291778e913d589c6347ba0a5d8c40cd94ce1
PedroLuizAP <pedrinho.apolinario@hotmail.com>
2022-10-12 15:47:51 -0300

    Finalizando o desafio

---
+++ challenges/4-fizz-buzz/c#/PedroLuizAP/Program.cs
@@ -0,0 +1,22 @@
+﻿int valorInteiro = 0;
+
+if (int.TryParse(args[0], out valorInteiro))
+{
+    for (int i = 1; i <= valorInteiro; i++)
+    {
+        string saida = string.Empty;
+
+        if (i % 3 != 0 && i % 5 != 0)
+        {
+            saida += $"{i}";
+        }
+        else
+        {
+            if (i % 3 == 0) saida += "Fizz";
+
+            if (i % 5 == 0) saida += "Buzz";
+        }
+
+        Console.WriteLine(saida);
+    }
+}
