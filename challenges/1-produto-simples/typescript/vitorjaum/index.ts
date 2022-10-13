33d0fda4720a3e260ff048dea0a09e4c51ab5663
João Vitor Pinheiro <47891679+vitorjaum@users.noreply.github.com>
2022-10-12 21:54:42 -0400

    Update index.ts
    add tests

--- challenges/1-produto-simples/typescript/vitorjaum/index.ts
+++ challenges/1-produto-simples/typescript/vitorjaum/index.ts
@@ -2,6 +2,7 @@ function calcNumbers(firstNumber: number, secondNumber: number) {
   return firstNumber * secondNumber;
 }
 
-const prod = calcNumbers(5, 5);
+const prod = calcNumbers(10, 3);
+const prod2 = calcNumbers(5, 5);
 
-console.log(`produto: ${prod}`);
+console.log(`produto 1: ${prod} \nproduto 2: ${prod2}`);
