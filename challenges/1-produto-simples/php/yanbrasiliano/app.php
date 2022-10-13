f6a1ff4f024530ffb4f662060c5a7ae7ff0a1b64
Yan Brasiliano Silva Penalva <yan.penalva@qintess.com>
2022-10-12 22:38:26 -0300

    Finalizando o desafio 1.

--- challenges/1-produto-simples/php/yanbrasiliano/app.php
+++ challenges/1-produto-simples/php/yanbrasiliano/app.php
@@ -11,4 +11,4 @@ function multiplication(int $valueA, int $valueB): int
 
 
 ## Verify if value is set correctly.
-echo $response = (isset($argv[1]) and isset($argv[2])) ? multiplication($argv[1], $argv[2]) : "Value not set, try again.";
+echo $response = (isset($argv[1]) && isset($argv[2])) ? 'Total value is: ' . multiplication($argv[1], $argv[2]) . PHP_EOL : "Value not set, try again." . PHP_EOL;
