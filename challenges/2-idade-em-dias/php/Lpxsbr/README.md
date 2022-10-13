b9ea7163595b58e95139f649c86de2f056b1a6d8
Kleber <anselmolopes.an@gmail.com>
2022-10-12 22:35:01 -0300

    finalizando desafio 2

---
+++ challenges/2-idade-em-dias/php/Lpxsbr/README.md
@@ -0,0 +1,22 @@
+# Submissão de Exercicio
+
+**Exercicio:** 2 - Idade em dias
+
+**Nickname:** LpxsBr
+
+**Nível Técnico:** - Estudante -
+
+**Empresa:** - Nenhuma -
+
+**Twitter**: 
+
+**Dificuldade de Resolução:** - Media -
+
+**Comentários:** achei curioso pensar nos casos em que usemos isso com valores reais de meses.. por ex.. um com 28 ou 29 dias, outros com 30 ou 31..
+
+**Como rodar o desafio**: 
+
+Use o comando abaixo:
+```bash
+php idade.php {argv[1]}
+```
---
+++ challenges/2-idade-em-dias/php/Lpxsbr/idade.php
@@ -0,0 +1,22 @@
+<?php
+if(isset($argv[1])) {
+
+    $idade = $argv[1];
+
+    $anos = $idade/365;
+    $meses = ($idade > (365)) ? (($anos)*$idade)/30 : $meses = $idade <= 365 ? 0 : $idade/30;
+    $idade = ($idade > (365)) ? (((($anos)*$idade)/30)-((int) $meses))*30 : $idade = $idade <= 365 ? 0 : ((($idade)/30)-((int) $meses))*30;
+
+    $frase1 = (int) $anos > 1 ? ' anos' : ' ano';
+    $frase2 = (int) $meses > 1 ? ' meses' : ' mês';
+    $frase3 = (int) $meses > 1 ? ' dias' : ' dias';
+
+    echo number_format($anos, 0).$frase1.PHP_EOL;
+    echo number_format($meses, 0).$frase2.PHP_EOL;
+    echo number_format($idade, 0).$frase3.PHP_EOL;
+
+} else {
+
+    echo 'sem valores informados, tente novamente';
+    
+}
